18417716c744478ea6a9a070b1f4cccf24918a91
HAREN PRABU <80617267+harenprabu@users.noreply.github.com>
2021-10-04 15:41:07 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req, res) => {
+res.setHeader('content-Type','text/html');
+let path = './views/';
+switch(req.url){
+    case '/kongu':
+        path +='index.html';
+        console.log("request by  kec")
+        break;
+    case '/skm':
+        path +='marketing.html';
+        console.log("request by skm")
+        break;
+    default:
+        path +='404.html';
+        break;
+}
+
+
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.end(data, 'utf-8');
+        }
+    })
+
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for  request on port 3000');
+});
